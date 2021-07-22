be2f9b8af8501aa70c61013ec3f388e0842b7a9b
SURBHI <81489001+surbhii25@users.noreply.github.com>
2021-07-22 19:26:49 +0530

    Update and rename DetectCycleInDirectedGraph.cpp to Detect Cycle in Directed Graph using DFS.cpp

---
+++ Detect Cycle in Directed Graph using DFS.cpp
@@ -0,0 +1,15 @@
+/*
+Depth First Traversal or DFS traversal can be used to detect a cycle in a Graph. DFS for a connected graph produces a tree. 
+There is a cycle in a graph only if there is a back edge present in the graph. 
+A back edge is an edge that is from a node to itself (self-loop) or one of its ancestor in the tree produced by DFS.
+*/
+
+
+
+/*
+
+*/
+
+/*
+
+*/
