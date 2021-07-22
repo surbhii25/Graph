721b9afc46c6c9171e24c427ab23ebb60099636e
SURBHI <81489001+surbhii25@users.noreply.github.com>
2021-07-22 14:09:15 +0530

    Update BFS.cpp

--- BFS.cpp
+++ BFS.cpp
@@ -11,6 +11,66 @@ BFS traversal of a graph is similar to that of the Level Order Traversal of Tree
   - If any of the adjacent vertex is not already visited, mark it visited and push it to the queue.
 */
 
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+    public:
+    //Function to return Breadth First Traversal of given graph.
+    vector<int>bfsOfGraph(int V, vector<int> adj[])
+    {
+        // Code here
+        vector<int> bfs;
+        vector<int> vis(V+1, 0);
+        queue<int> q;
+        q.push(0);
+        vis[0] = 1;
+
+        while(!q.empty()) {
+            int node = q.front();
+            q.pop();
+            bfs.push_back(node);
+
+            for(auto it : adj[node]) {
+                if(!vis[it]) {
+                    q.push(it);
+                    vis[it] = 1;
+                }
+            }
+        }
+
+        return bfs;
+    }
+};
+
+int main(){
+    int tc;
+    cin >> tc;
+    while(tc--){
+      int V, E;
+      cin >> V >> E;
+
+        vector<int> adj[V];
+
+        for(int i = 0; i < E; i++)
+        {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            // adj[v].push_back(u);
+        }
+            // string s1;
+            // cin>>s1;
+        Solution obj;
+        vector<int>ans=obj.bfsOfGraph(V, adj);
+        for(int i=0;i<ans.size();i++){
+            cout<<ans[i]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
 
 
 /*
