42fedaa00745352532f4f76dc327d000b82a726b
SURBHI <81489001+surbhii25@users.noreply.github.com>
2021-07-22 20:41:29 +0530

    Update Detect Cycle in Directed Graph using DFS.cpp

--- Detect Cycle in Directed Graph using DFS.cpp
+++ Detect Cycle in Directed Graph using DFS.cpp
@@ -19,10 +19,86 @@ The edge that connects the current vertex to the vertex in the recursion stack i
    Else if for all vertices the function returns false return false.
 */
 
+#include<bits/stdc++.h>
+using namespace std;
 
+class Solution
+{
+    public:
+    
+        bool cycleCheck(int node, vector<int> adj[], int vis[], int dfsVis[]) {
+            vis[node] = 1;
+            dfsVis[node] = 1;
+
+            for(auto it : adj[node]) {
+                if(!vis[it]) {
+                    if(cycleCheck(it, adj, vis, dfsVis))
+                        return true;
+                }
+                else if(dfsVis[it])
+                    return true;
+            }
+
+            dfsVis[node] = 0;
+            return false;
+        }
+    
+	//Function to detect cycle in a directed graph.
+        bool isCyclic(int V, vector<int> adj[]) 
+        {
+            // code here
+            int vis[V], dfsVis[V];
+            memset(vis, 0, sizeof vis);
+            memset(dfsVis, 0, sizeof dfsVis);
+
+            for(int i=0; i<V; i++) {
+                if(!vis[i]) {
+                    if(cycleCheck(i, adj, vis, dfsVis)) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+
+        }
+};
+
+int main()
+{
+    
+    int t;
+    cin >> t;
+    while(t--)
+    {
+    	int V, E;
+    	cin >> V >> E;
+
+    	vector<int> adj[V];
+
+    	for(int i = 0; i < E; i++)
+    	{
+    		int u, v;
+    		cin >> u >> v;
+    		adj[u].push_back(v);
+    	}
+
+    	Solution obj;
+    	cout << obj.isCyclic(V, adj) << "\n";
+    }
+
+    return 0;
+}
 
 /*
+For Input:
+4 4
+0 1
+1 2
+2 3
+3 3
 
+Your Output is: 
+1
 */
 
 /*
