ad9e1bf9b11678d93b1c72ce15dc63d4e6d97c3e
SURBHI <81489001+surbhii25@users.noreply.github.com>
2021-07-22 13:21:10 +0530

    Update AdjacencyList.cpp

--- Graph Representation/AdjacencyList.cpp
+++ Graph Representation/AdjacencyList.cpp
@@ -1,1 +1,62 @@
+// A simple representation of graph using STL
+#include<bits/stdc++.h>
+using namespace std;
 
+// A utility function to add an edge in an undirected graph.
+void addEdge(vector<int> adj[], int u, int v)
+{
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
+
+// A utility function to print the adjacency list representation of graph.
+void printGraph(vector<int> adj[], int V)
+{
+    for (int v = 0; v < V; ++v)
+    {
+        cout << "\n Adjacency list of vertex "
+             << v << "\n head ";
+        for (auto x : adj[v])
+            cout << "-> " << x;
+        printf("\n");
+    }
+}
+
+// Driver code
+int main()
+{
+    int V = 5;
+    vector<int> adj[V];                         // The vector implementation has advantages of cache friendliness.
+    addEdge(adj, 0, 1);
+    addEdge(adj, 0, 4);
+    addEdge(adj, 1, 2);
+    addEdge(adj, 1, 3);
+    addEdge(adj, 1, 4);
+    addEdge(adj, 2, 3);
+    addEdge(adj, 3, 4);
+    printGraph(adj, V);
+    return 0;
+}
+
+
+/* OUTPUT:
+Adjacency list of vertex 0
+head -> 1-> 4
+
+Adjacency list of vertex 1
+head -> 0-> 2-> 3-> 4
+
+Adjacency list of vertex 2
+head -> 1-> 3
+
+Adjacency list of vertex 3
+head -> 1-> 2-> 4
+
+Adjacency list of vertex 4
+head -> 0-> 1-> 3
+*/
+
+
+// Pros: Saves space O(|V|+|E|). In the worst case, there can be C(V, 2) number of edges in a graph thus consuming O(V^2) space. Adding a vertex is easier.
+
+// Cons: Queries like whether there is an edge from vertex u to vertex v are not efficient and can be done O(V).
