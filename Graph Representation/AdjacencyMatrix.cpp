d50c8786560b41c954d0151d873461208e2463e0
SURBHI <81489001+surbhii25@users.noreply.github.com>
2021-07-22 13:07:14 +0530

    Update AdjacencyMatrix.cpp

--- Graph Representation/AdjacencyMatrix.cpp
+++ Graph Representation/AdjacencyMatrix.cpp
@@ -19,4 +19,6 @@ int main() {
     A[x][y] = true;                             //Mark the edges from vertex x to vertex y
 }
 
-// Space Complexity: O(NxN)
+// Pros: Representation is easier to implement and follow. Removing an edge takes O(1) time. Queries like whether there is an edge from vertex 'u' to vertex 'v' are efficient and can be done O(1).
+
+// Cons: Consumes more space O(V^2). Even if the graph is sparse(contains less number of edges), it consumes the same space. Adding a vertex is O(V^2) time.
