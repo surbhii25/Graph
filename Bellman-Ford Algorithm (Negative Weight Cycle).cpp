bd544e702ea4b967ac0cca86dc06d4dfa75746d6
SURBHI <81489001+surbhii25@users.noreply.github.com>
2021-07-24 02:05:34 +0530

    Update Bellman-Ford Algorithm (Negative Weight Cycle).cpp

--- Bellman-Ford Algorithm (Negative Weight Cycle).cpp
+++ Bellman-Ford Algorithm (Negative Weight Cycle).cpp
@@ -1,5 +1,5 @@
 /*
-
+Given a graph and a source vertex src in graph, find shortest paths from src to all vertices in the given graph. The graph may contain negative weight edges. 
 */
 
 #include<bits/stdc++.h>
