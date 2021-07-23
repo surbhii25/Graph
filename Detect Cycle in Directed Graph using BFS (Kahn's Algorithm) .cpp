2eb7af05cd0c5fefdad193316a7ea83fd167503d
SURBHI <81489001+surbhii25@users.noreply.github.com>
2021-07-23 14:47:05 +0530

    Update Detect Cycle in Directed Graph using BFS (Kahn's Algorithm) .cpp

--- Detect Cycle in Directed Graph using BFS (Kahn's Algorithm) .cpp
+++ Detect Cycle in Directed Graph using BFS (Kahn's Algorithm) .cpp
@@ -1,7 +1,7 @@
 /*
 We are gonna use Kahn's Algorithm for Topological Sort in this question. Topological Sorting for a graph is only possible if the graph is a DAG.
 If we are unable to generate Topological Sort in this question then we can say the graph is not DAG, hence cycle is present in the graph.
-Basically, we are doing reverse logic to do this ques.
+Basically, we are using reverse logic to do this ques.
 */
 
 #include<bits/stdc++.h>
