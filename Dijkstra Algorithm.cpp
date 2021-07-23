0d452071dab47a13bee020aeb9412911c2d87854
SURBHI <81489001+surbhii25@users.noreply.github.com>
2021-07-24 00:22:26 +0530

    Update Dijkstra Algorithm.cpp

--- Dijkstra Algorithm.cpp
+++ Dijkstra Algorithm.cpp
@@ -33,10 +33,95 @@ Below is the complete algorithm using priority_queue(min heap) to implement Dijk
 Note: The Dijkstra's Algorithm doesn't work in the case when the Graph has negative edge weight.
 */
 
+#include<bits/stdc++.h>
+using namespace std;
+
+# define INF INT_MAX
+typedef pair<int,int> iPair;
+
+class Solution
+{
+	public:
+	//Function to find the shortest distance of all the vertices
+    //from the source vertex S.
+    vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
+    {
+        // Code here
+        // priority_queue <Type, vector<Type>, ComparisonType > min_heap;
+        priority_queue <iPair, vector<iPair>, greater<iPair> > pq;
+        // pq(min-heap) : In pair => (dist,from)
+        
+        vector<int> dist(V, INF);
+        
+        dist[S] = 0;
+        pq.push(make_pair(0, S));
+        
+        while(!pq.empty()) {
+            
+            int u = pq.top().second;
+            pq.pop();
+            
+            for(auto it : adj[u]) {
+                
+                int v = it[0];
+                int weight = it[1];
+                
+                if(dist[v] > dist[u] + weight) {
+                    dist[v] = dist[u] + weight;
+                    pq.push(make_pair(dist[v], v));
+                }
+                
+            }
+            
+        }
+            
+        return dist;
+    }
+};
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--) {
+        int V, E;
+        cin >> V >> E;
+        vector<vector<int>> adj[V];
+        int i=0;
+        while (i++<E) {
+            int u, v, w;
+            cin >> u >> v >> w;
+            vector<int> t1,t2;
+            t1.push_back(v);
+            t1.push_back(w);
+            adj[u].push_back(t1);
+            t2.push_back(u);
+            t2.push_back(w);
+            adj[v].push_back(t2);
+        }
+        int S;
+        cin>>S;
+        
+        Solution obj;
+    	vector<int> res = obj.dijkstra(V, adj, S);
+    	
+    	for(int i=0; i<V; i++)
+    	    cout<<res[i]<<" ";
+    	cout<<endl;
+    }
+
+    return 0;
+}
 
 
 /*
+For Input:
+2 1
+0 1 9
+0
 
+Your Output is: 
+0 9 
 */
 
 /*
