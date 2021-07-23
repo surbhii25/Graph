8fb8526f079738feb8a27ed450af1bfd96f045c3
SURBHI <81489001+surbhii25@users.noreply.github.com>
2021-07-24 02:06:00 +0530

    Update Bellman-Ford Algorithm (Positive Weight Cycle).cpp

--- Bellman-Ford Algorithm (Positive Weight Cycle).cpp
+++ Bellman-Ford Algorithm (Positive Weight Cycle).cpp
@@ -1,5 +1,5 @@
 /*
-Problem: Given a graph and a source vertex src in graph, find shortest paths from src to all vertices in the given graph. The graph may contain negative weight edges.
+Problem: Given a graph and a source vertex src in graph, find shortest paths from src to all vertices in the given graph.
 
 Dijkstra doesn't work for Graphs with negative weight edges, Bellman-Ford works for such graphs. Bellman-Ford is also simpler than Dijkstra and suites well for distributed systems.
 But time complexity of Bellman-Ford is O(VE), which is more than Dijkstra.
