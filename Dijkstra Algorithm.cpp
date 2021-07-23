e1ce61711021267860085752726231204d2fa030
SURBHI <81489001+surbhii25@users.noreply.github.com>
2021-07-23 21:34:49 +0530

    Update Dijkstra Algorithm.cpp

--- Dijkstra Algorithm.cpp
+++ Dijkstra Algorithm.cpp
@@ -9,5 +9,6 @@
 */
 
 /*
-
+Time Complexity: The time complexity of the Dijkstra's Algorithm when implemented using a min heap is O(E * logV), where E is the number of Edges and V is the number of vertices.
+Space Complexity: O(N) + O(N) because we used dist array and adj list.
 */
