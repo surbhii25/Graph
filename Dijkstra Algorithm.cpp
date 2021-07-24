43a97afeffa394fc926240b44eedc9afcd598d0f
SURBHI <81489001+surbhii25@users.noreply.github.com>
2021-07-24 22:05:23 +0530

    Update Dijkstra Algorithm.cpp

--- Dijkstra Algorithm.cpp
+++ Dijkstra Algorithm.cpp
@@ -130,5 +130,5 @@ Your Output is:
 Time Complexity: O(V * logV) (with the use of Fibonacci heap)
 The time complexity of the Dijkstra's Algorithm when implemented using a min heap is O(E * logV), where E is the number of Edges and V is the number of vertices.
 
-Space Complexity: O(N) + O(N) because we used dist array and adj list.
+Space Complexity: O(V) + O(V) because we used dist array and adj list.
 */
