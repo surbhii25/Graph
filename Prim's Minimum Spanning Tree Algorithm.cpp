6528516ea4c92761e9cc8fc883e8b58f77e7160b
SURBHI <81489001+surbhii25@users.noreply.github.com>
2021-07-24 12:37:47 +0530

    Update Prim's Minimum Spanning Tree Algorithm.cpp

--- Prim's Minimum Spanning Tree Algorithm.cpp
+++ Prim's Minimum Spanning Tree Algorithm.cpp
@@ -36,6 +36,86 @@ the key value for these vertices indicate the minimum weight edges connecting th
 
 
 
+typedef pair<int,int> iPair;
+
+class Solution
+{
+	public:
+	//Function to find sum of weights of edges of the Minimum Spanning Tree.
+    int spanningTree(int V, vector<vector<int>> adj[])
+    {
+        // code here
+        vector<int> key(V, INT_MAX);
+        vector<bool> mstSet(V, false);
+        //vector<int> parent(V, -1);
+        
+        priority_queue<iPair, vector<iPair>, greater<iPair> > pq;
+        
+        key[0] = 0;
+        //parent[0] = -1;
+        pq.push({0, 0});
+        
+        int sum = 0;
+        
+        while(!pq.empty()) {
+            int u = pq.top().second;
+            pq.pop();
+            
+            if(!mstSet[u]) {
+                mstSet[u] = true;
+                sum += key[u];
+                
+                for(auto it : adj[u]) {
+                
+                    int v = it[0];
+                    int w = it[1];
+                    
+                    if(mstSet[v] == false && w < key[v]) {
+                        key[v] = w;
+                        //parent[v] = u;
+                        pq.push({w, v});
+                    }
+                    
+                }
+                
+            }
+            
+        }
+      
+        return sum;
+    }
+};
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--) {
+        int V, E;
+        cin >> V >> E;
+        vector<vector<int>> adj[V];
+        int i=0;
+        while (i++<E) {
+            int u, v, w;
+            cin >> u >> v >> w;
+            vector<int> t1,t2;
+            t1.push_back(v);
+            t1.push_back(w);
+            adj[u].push_back(t1);
+            t2.push_back(u);
+            t2.push_back(w);
+            adj[v].push_back(t2);
+        }
+        
+        Solution obj;
+    	cout << obj.spanningTree(V, adj) << "\n";
+    }
+
+    return 0;
+}
+
+  // } Driver Code Ends
+
 /*
 For Input:
 3 3
@@ -44,7 +124,7 @@ For Input:
 0 2 1
 
 Your Output is: 
-6
+4
 */
 
 /*
