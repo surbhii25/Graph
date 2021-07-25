1b716374bfcfd13032d4191356aebc581c6df028
SURBHI <81489001+surbhii25@users.noreply.github.com>
2021-07-25 20:23:57 +0530

    Update Kruskal's Algorithm (Reverse Delete Algorithm for MST).cpp

--- Kruskal's Algorithm (Reverse Delete Algorithm for MST).cpp
+++ Kruskal's Algorithm (Reverse Delete Algorithm for MST).cpp
@@ -1,4 +1,21 @@
 /*
+Reverse Delete algorithm is closely related to Kruskal's algorithm.
+
+In Kruskal's algorithm what we do is : Sort edges by increasing order of their weights. After sorting, we one by one pick edges in increasing order. 
+We include current picked edge if by including this in spanning tree not form any cycle until there are V-1 edges in spanning tree, where V = number of vertices.
+
+In Reverse Delete algorithm, we sort all edges in decreasing order of their weights. After sorting, we one by one pick edges in decreasing order. 
+We include current picked edge if excluding current edge causes disconnection in current graph. The main idea is delete edge if its deletion does not lead to disconnection 
+of graph.
+
+# Algorithm:
+1) Sort all edges of graph in non-increasing order of edge weights.
+
+2) Initialize MST as original graph and remove extra edges using step 3.
+
+3) Pick highest weight edge from remaining edges and check if deleting the edge disconnects the graph or not.
+    - If disconnects, then we don't delete the edge.
+    - Else we delete the edge and continue. 
 
 */
 
@@ -108,5 +125,7 @@ Your Output is:
 */
 
 /*
-
+TC: O(E * log E) + O(E * O(4a)) = O(E * log E)
+Here, a = alpha; O(4a) is complexity of disjoint set used.
+SC: O(E) + O(V) + O(V) = O(V)
 */
